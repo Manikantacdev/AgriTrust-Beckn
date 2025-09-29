6e437600d9db443a2ad8c27efca59f6a2672ec27
Manikanta-web-developer <manigedda731@gmail.com>
2025-09-29 22:05:58 +0530

    allowed host

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
+    allowedHosts: ["agritrust-beckn.onrender.com"],
     host: true,
-    allowedHosts: "all",
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
